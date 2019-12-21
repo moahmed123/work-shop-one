70fa1e3aba30dc85f51d067cd7f462d6a23141f4
“amirahmed33” <miroglobal2015@gmail.com>
2019-12-21 13:16:01 +0200

    pre final

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-$(window).load(function(){
+$(window).load(function () {
     var $container = $('.portfolioContainer');
     $container.isotope({
         filter: '*',
@@ -8,11 +8,11 @@ $(window).load(function(){
             queue: false
         }
     });
- 
-    $('.portfolioFilter a').click(function(){
+
+    $('.portfolioFilter a').click(function () {
         $('.portfolioFilter .current').removeClass('current');
         $(this).addClass('current');
- 
+
         var selector = $(this).attr('data-filter');
         $container.isotope({
             filter: selector,
@@ -21,7 +21,26 @@ $(window).load(function(){
                 easing: 'linear',
                 queue: false
             }
-         });
-         return false;
-    }); 
+        });
+        return false;
+    });
+    /* Start Back to Top Button */
+    var btn = $('#backtotop');
+
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 300) {
+            btn.addClass('show');
+        } else {
+            btn.removeClass('show');
+        }
+    });
+
+    btn.on('click', function (e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: 0
+        }, '300');
+    });
+    /* End Back to Top Button */
+
 });
