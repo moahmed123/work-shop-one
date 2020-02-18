7b3ef35983eb5a3ca825eddc59ae1626b7a2aa1f
Mohamed Alaa <mohamedalaaabas93@gmail.com>
2020-02-18 19:09:33 +0200

    imporved js files, animation for site and blog

--- js/main.js
+++ js/main.js
@@ -4,12 +4,13 @@
 'use strict';
 
 $(window).on("load", function () {
-    $(".Loading-Page").fadeOut(100)    
+    $(".Loading-Page").fadeOut(100)
 });
 
 $(function () {
     // Animation scroll.
-    $('html').smoothScroll(950);
+    $('html').smoothScroll(1950);
+
     var typed = new Typed('#typed', {
         stringsElement: '#typed-strings',
         typeSpeed: 70,
@@ -46,6 +47,50 @@ $(function () {
         itemSelector: '.grid-item',
         layoutMode: 'fitRows'
     });
-    $grid.isotope({ filter: '.metal.transition' });
+    // $grid.isotope({ filter: '.metal.transition' });
+
+    
+    $(window).on('scroll', function () {
+        // Cach For Scroll Page 
+        const ScrollCount = $(window).scrollTop(),
+            AboutScection = $('#about').offset().top,
+            ServiceScection = $('#service').offset().top,
+            PortfolioScection = $('#portfolio').offset().top,
+            ContactScection = $('#contact').offset().top,
+            BlogScection = $('#blog').offset().top;
+
+        // Header Scroll Animation     
+        if (ScrollCount > 50) {
+            $("nav.navbar").addClass('scroll');
+        } else {
+            $("nav.navbar").removeClass('scroll');
+        }
+        // Active Nav List When Scroll 
+        if( ScrollCount >= AboutScection - 20){
+            ActiveNavigation('#About_nav'); // About Page 
+        }
+        if (ScrollCount >= ServiceScection - 20){
+            ActiveNavigation('#Service_nav'); // Service Page
+        }
+        if (ScrollCount >= PortfolioScection - 20){
+            ActiveNavigation('#Portfolio_nav'); // Portfolio Page 
+        }        
+        if(ScrollCount >= BlogScection - 20){
+            ActiveNavigation('#Blog_nav'); // Blog Page
+        }
+        if(ScrollCount >= ContactScection - 20){
+            ActiveNavigation('#Contact_nav'); // Contact Page
+        }
+        if(ScrollCount < 200 ){
+            ActiveNavigation('#Home_nav'); // Home Page
+        }        
+
+    })
+
+    function ActiveNavigation(ElementAction){
+        $('nav .navbar-nav li').removeClass('active')
+        $(ElementAction).addClass('active');
+
+    }
 
 });
