4aaf410515c8bcc4cecd4ac3bb37e9e41d901993
amirahmed33 <miroglobal2015@gmail.com>
2020-02-23 16:41:48 +0200

    final

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # work-shop-one
 # mAlaa edit on 3.12.2019  
-# Amir edit on 27.12.2019   
+# Amir edit on 23.02.2020   
--- js/main.js
+++ js/main.js
@@ -4,12 +4,13 @@
 'use strict';
 
 $(window).on("load", function () {
-    $(".Loading-Page").fadeOut(100)    
+    $(".loading-animation").fadeOut(100)
 });
 
 $(function () {
     // Animation scroll.
-    $('html').smoothScroll(950);
+    $('html').smoothScroll(1950);
+
     var typed = new Typed('#typed', {
         stringsElement: '#typed-strings',
         typeSpeed: 70,
@@ -20,32 +21,55 @@ $(function () {
         // fadeOut: true
     });
 
-    // Wrap every letter in a span [Loading]
-    var textWrapper = document.querySelector('.ml2');
-    textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+    //Portfolio 
+    // $('.grid').isotope({
+    //     // options
+    //     itemSelector: '.grid-item',
+    //     layoutMode: 'fitRows'
+    // });
+    // // $grid.isotope({ filter: '.metal.transition' });
 
-    anime.timeline({ loop: true })
-        .add({
-            targets: '.ml2 .letter',
-            scale: [4, 1],
-            opacity: [0, 1],
-            translateZ: 0,
-            easing: "easeOutExpo",
-            duration: 950,
-            delay: (el, i) => 70 * i
-        }).add({
-            targets: '.ml2',
-            opacity: 0,
-            duration: 1000,
-            easing: "easeOutExpo",
-            delay: 1000
-        });
+    
+    $(window).on('scroll', function () {
+        // Cach For Scroll Page 
+        const ScrollCount = $(window).scrollTop(),
+            Navbar = $("nav.navbar"),
+            AboutScection = $('#about').offset().top,
+            ServiceScection = $('#service').offset().top,
+            PortfolioScection = $('#portfolio').offset().top,
+            ContactScection = $('#contact').offset().top,
+            BlogScection = $('#blog').offset().top;
 
-    $('.grid').isotope({
-        // options
-        itemSelector: '.grid-item',
-        layoutMode: 'fitRows'
-    });
-    $grid.isotope({ filter: '.metal.transition' });
+        // Header Scroll Animation     
+        if (ScrollCount > 50) {
+            Navbar.addClass('scroll');
+        } else {
+            Navbar.removeClass('scroll');
+        }
+        // Active Nav List When Scroll 
+        if( ScrollCount >= AboutScection - 20){
+            ActiveNavigation('#About_nav'); // About Page 
+        }
+        if (ScrollCount >= ServiceScection - 20){
+            ActiveNavigation('#Service_nav'); // Service Page
+        }
+        if (ScrollCount >= PortfolioScection - 20){
+            ActiveNavigation('#Portfolio_nav'); // Portfolio Page 
+        }        
+        if(ScrollCount >= BlogScection - 20){
+            ActiveNavigation('#Blog_nav'); // Blog Page
+        }
+        if(ScrollCount >= ContactScection - 20){
+            ActiveNavigation('#Contact_nav'); // Contact Page
+        }
+        if(ScrollCount < 200 ){
+            ActiveNavigation('#Home_nav'); // Home Page
+        }        
 
+    })
+
+    function ActiveNavigation(ElementAction){
+        $('nav .navbar-nav li').removeClass('active')
+        $(ElementAction).addClass('active');
+    }
 });
